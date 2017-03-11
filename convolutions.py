02b181a29cfc761e1341d90c479697a62efa947e
LIKANblk <likan26.09@yandex.ru>
2017-03-11 15:25:23 +0300

    recursive function #1: Not working

--- convolutions.py
+++ convolutions.py
@@ -3,6 +3,7 @@ from mne.io import read_raw_fif
 from mne.channels import find_layout,read_ch_connectivity
 from mne.datasets import sample
 from scipy.sparse import find
+import numpy as np
 
 class convolution_neuromag:
     #
@@ -16,32 +17,52 @@ class convolution_neuromag:
         neuromag = read_raw_fif(sample.data_path() +
                           '/MEG/sample/sample_audvis_raw.fif')
         self.layout = find_layout(neuromag.info, ch_type=self.sensor_type)
-
+        
         self.neighboring,self.ch_names = read_ch_connectivity(neighboring_filename, picks=None)
+        self.num_channels = len(self.ch_names)
         #self.neighboring = neighboring.toarray()
 
     # def visualise_sensors(self):
 
-    def cacl_edges(self):
+    def cacl_edges_matrix(self):
         #We need only upper part of connectivity matrix
         pos = self.layout.pos[:, 0:1]
-        res=[]
         neib_array = self.neighboring.toarray()
+        edges_matrix = np.empty(self.neighboring.shape,dtype=object)
         for x in xrange(len(neib_array)):
-            for y in xrange(x,len(neib_array)):
+            for y in xrange(x+1,len(neib_array)):
               if neib_array[x,y]:
                 edge = (pos[x][0]-pos[y][0],pos[x][1]-pos[y][1])
-                res.extend((self.ch_names[x],self.ch_names[y],edge))
+                edges_matrix[x,y]=edge
 
 
-    def get_1Dconvolutions(self,conv_length=2):
-        pos = self.layout.pos[:,0:1] #get only (x,y) coordinates (without sensors width and height)
-        num_channels = pos.shape[0]
-        index_neighboring = find(self.neighboring) #tuple of non-zero elems
-        for ch in xrange(num_channels):
-            for
 
 
+
+    def get_1Dconvolutions(self,conv_length):
+
+        def _find_next_neighbor(self, row_of_edge_matrix, avail_channels):
+            # TODO Rewrite as lambda expression
+            for ch_number in xrange(row_of_edge_matrix):
+                if (row_of_edge_matrix[ch_number]):
+                    return avail_channels[ch_number]
+        def recursive_search(accumulator,conv_length,avail_channels,edge_matrix):
+
+            for ch_index, ch in enumerate(avail_channels):
+                if conv_length == 1:
+                    
+                    return accumulator
+
+                next_ch = self._find_next_neibor(edge_matrix[ch],avail_channels)
+                accumulator.extend(next_ch)
+                avail_channels.remove(next_ch)
+                edge_matrix = np.delete(edge_matrix, ch_index, 0)
+                edge_matrix = np.delete(edge_matrix, ch_index, 1)
+
+                recursive_search(, conv_length-1, avail_channels, edge_matrix)
+
+        edge_matrix = self.cacl_edges_matrix()
+        recursive_search([], conv_length, self.ch_names, edge_matrix)
 
 
 
