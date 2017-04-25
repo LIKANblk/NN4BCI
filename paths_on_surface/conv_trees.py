b45455a7ff34ef5dd778e38dc1cc315b8e32afd1
Petr Sorokoumov <petr.sorokoumov@gmail.com>
2017-04-25 13:52:21 +0300

    Triangle routing coded but not yet fully debugged

--- paths_on_surface/conv_trees.py
+++ paths_on_surface/conv_trees.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import Queue as qu
+import copy
 
 # calculate 3d direction of convolution points
 # TODO: remake to process any number of points in a convolution
@@ -131,18 +132,35 @@ def initialize_queue_by_vertex(v, f, init_v, target_v):
         angle_12 = math.acos(np.dot(ort1,ort2))
         # calculate flat coordinates of each vertex processed
         v1_flat = np.array([len1*math.cos(curr_angle), len1*math.sin(curr_angle)])
-        curr_angle += angle_12
+        curr_angle -= angle_12
         v2_flat = np.array([len2*math.cos(curr_angle), len2*math.sin(curr_angle)])
         # calculate measure of distance from v1-v2 to target
         dist = np.linalg.norm((v[first_v]+v[second_v])/2 - v[target_v])
-        variants.put((dist, [first_v, second_v, v1_flat, v2_flat, v1_flat, v2_flat, [[first_v, second_v]]]))
+        variants.put((dist, [first_v, second_v, v1_flat, v2_flat, v1_flat, v2_flat, [[first_v, second_v, v1_flat, v2_flat]]]))
     return variants
 
+def rel_angle(v, base_v):
+    len_b = np.linalg.norm(base_v)
+    len_v = np.linalg.norm(v)
+    return math.asin((v[1]*base_v[0]-v[0]*base_v[1])/len_b/len_v)
+
+assert(math.fabs(rel_angle(np.array([1,1]), np.array([2,2])))<1e-8)
+assert(math.fabs(rel_angle(np.array([1,1]), np.array([0,1]))+math.pi/4)<1e-8)
+
+def print_queue(q):
+    lst = []
+    for i in q.queue:
+        lst.append((i[0],i[1][0],i[1][1]))
+    lst.sort(key=lambda v: v[0])
+    print(lst)
+
 # one step in depth for dfs
-# variants: priority queue of (target_dist, [v1 v2 v1_flat(x,y) v2_flat(x,y) angle_start_flat(x,y) angle_end_flat(x,y) stack])
+# variants: priority queue of 
+#   (target_dist, [v1 v2 v1_flat(x,y) v2_flat(x,y) angle_start_flat(x,y) angle_end_flat(x,y) stack])
 # init_vertex,target_vertex: positions
 # return: (False, new_variants, []) or (True, [], answer)
 def dfs_step(v, f, variants, init_vertex, target_vertex):
+    eps = 1e-6
     # if there are no any more variants, return 0
     if variants.empty():
         return (False, variants, [])
@@ -157,7 +175,16 @@ def dfs_step(v, f, variants, init_vertex, target_vertex):
     old_stack = next_data[1][6]
     # check if the target is achieved; if yes, return result
     if v1==target_vertex or v2==target_vertex:
-        return (True, [], old_stack)
+        # if the target point is in angle
+        result_flat = v1_flat
+        if v2==target_vertex:
+            result_flat = v2_flat
+        cross1 = a1_flat[0]*result_flat[1]-a1_flat[1]*result_flat[0]
+        cross2 = a2_flat[1]*result_flat[0]-a2_flat[0]*result_flat[1]
+        if cross1>=-eps and cross2>=-eps:
+            return (True, [], old_stack)
+        else:
+            return (False, variants, [])
     # if the target is not achieved yet, expang two edges
     # 0. Find new vertex in f
     found_v = [c_f[2] for c_f in f if c_f[0]==v2 and c_f[1]==v1]
@@ -168,9 +195,53 @@ def dfs_step(v, f, variants, init_vertex, target_vertex):
     assert(len(found_v)==1) # else the surface is non-manifold
     v3 = found_v[0]    
     # 1. Calculate the new vertex position
-#    l13 = np.linalg.norm(v[v3] - v[v1])
-#    angle_213 = 
-    # TODO: continue
+    l13 = np.linalg.norm(v[v3] - v[v1])
+    l12 = np.linalg.norm(v[v2] - v[v1])
+    angle_213 = math.acos(np.dot(v[v3] - v[v1], v[v2] - v[v1])/l13/l12)
+    fv12 = v2_flat-v1_flat
+    fv12_nrm = fv12 / l12
+    fv13_nrm = np.array([fv12_nrm[0]*math.cos(angle_213)-fv12_nrm[1]*math.sin(angle_213),
+                         fv12_nrm[0]*math.sin(angle_213)+fv12_nrm[1]*math.cos(angle_213)])
+    v3_flat = v1_flat + fv13_nrm*l13
+    # 2. Calculate new variants
+    # [v1 v2 v1_flat(x,y) v2_flat(x,y) angle_start_flat(x,y) angle_end_flat(x,y), edge_stack]
+    # calculate flat angles for corresponding vertices
+    anglev3 = 0
+    old_angle1 = rel_angle(a1_flat,v3_flat)
+    old_angle2 = rel_angle(a2_flat,v3_flat)
+    anglev1 = rel_angle(v1_flat,v3_flat)
+    anglev2 = rel_angle(v2_flat,v3_flat)
+    # add edge 13 if it is possible
+    if v1_flat[0]*v3_flat[1]-v1_flat[1]*v3_flat[0] < -eps:
+        # calculate boarding points for angles
+        angle_list = [(old_angle1, a1_flat),
+                      (old_angle2, a2_flat),
+                      (anglev1, v1_flat),
+                      (anglev3, v3_flat)]
+        angle_list.sort(key = lambda val: val[0]) # new angles are in angle_list[2][1] and [1][1]
+        target_dist_13 = np.linalg.norm((v[v1]+v[v3])/2 - v[target_vertex])
+        stack_13 = old_stack[:]
+        stack_13.append([v1,v3, angle_list[2][1], angle_list[1][1]])
+        variants.put((target_dist_13, [v1, v3, 
+                                       v1_flat, v3_flat, 
+                                       angle_list[2][1], angle_list[1][1], 
+                                       stack_13]))
+    # add edge 32 if it is possible
+    if v3_flat[0]*v2_flat[1]-v3_flat[1]*v2_flat[0]<-eps:
+        # calculate boarding points for angles
+        angle_list = [(old_angle1, a1_flat),
+                      (old_angle2, a2_flat),
+                      (anglev3, v3_flat),
+                      (anglev2, v2_flat)]
+        angle_list.sort(key = lambda val: val[0]) # new angles are in angle_list[2][1] and [1][1]
+        target_dist_32 = np.linalg.norm((v[v2]+v[v3])/2 - v[target_vertex])
+        #print('DISTS: ',v2, v3, (v[v2]+v[v3])/2, v[target_vertex], target_dist_32)
+        stack_32 = old_stack[:]
+        stack_32.append([v3,v2, angle_list[2][1], angle_list[1][1]])
+        variants.put((target_dist_32, [v3, v2,
+                                       v3_flat, v2_flat,
+                                       angle_list[2][1], angle_list[1][1], 
+                                       stack_32]))
     return (False, variants, [])
 
 # tests
@@ -178,8 +249,8 @@ def dfs_step(v, f, variants, init_vertex, target_vertex):
 # create test array of convolutions (simplest form)
 def gen_test_array(N):
     # vertices
-    xc = np.repeat(np.arange(1,N+1),N)
-    yc = np.reshape(np.repeat(np.asmatrix(np.arange(1,N+1)), N, 0), (1, N*N))
+    xc = np.repeat(np.arange(1.0,float(N+1)),N)
+    yc = np.reshape(np.repeat(np.asmatrix(np.arange(1.0,float(N+1))), N, 0), (1, N*N))
     vs = np.column_stack([xc, np.asarray(yc)[0], np.repeat([0], N*N)])
     # faces: temporary manual generation instead of Delaunay generation
     start_v = [i*N+j for j in range(0,N-1) for i in range(0,N-1)]
@@ -187,11 +258,24 @@ def gen_test_array(N):
     fs.extend([[v, v+1+N, v+N] for v in start_v])
     return (vs, fs)
 
-test_N = 4
+test_N = 8
+start_v = 15
+end_v = 40
 (test_v, test_fs) = gen_test_array(test_N)
-init_vars = initialize_queue_by_vertex(test_v, test_fs, 1, 14)
-dfs_step(test_v, test_fs, init_vars, 1, 14)
+variants = initialize_queue_by_vertex(test_v, test_fs, start_v, end_v)
+res = False
+#while res==False and not variants.empty():
+[res, variants, answ] = dfs_step(test_v, test_fs, variants, start_v, end_v)
+[res, variants, answ] = dfs_step(test_v, test_fs, variants, start_v, end_v)
+[res, variants, answ] = dfs_step(test_v, test_fs, variants, start_v, end_v)
+[res, variants, answ] = dfs_step(test_v, test_fs, variants, start_v, end_v)
+[res, variants, answ] = dfs_step(test_v, test_fs, variants, start_v, end_v)
+[res, variants, answ] = dfs_step(test_v, test_fs, variants, start_v, end_v)
+[res, variants, answ] = dfs_step(test_v, test_fs, variants, start_v, end_v)
+#print_queue(variants)
+#print(answ)
 
+(test_v, test_fs) = gen_test_array(4)
 test_convs = np.array([[0,1,2], [4,5,6], [9,10,11], [12,13,14], [3,7,11], [7,14,15]])
 res_seq = make_conv_trees(test_v, test_convs, 3, 0.1, 0.1)
 assert(res_seq==[[0,1,3]])
