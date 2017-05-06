60f98d8fd4aaafa30fc0585fbaec58c2e78b0bbb
bogdan <kozyrskiy@gmail.com>
2017-05-06 23:28:33 +0300

    rewrited alex feature extraction

--- AlexPreprocess.py
+++ AlexPreprocess.py
@@ -5,9 +5,9 @@ from itertools import combinations
 from Data import *
 from convolutions import *
 
-def get_convovled_data(data,convolutions_1d):
-    for conv in convolutions_1d:
-        yield data[:,conv,:]
+#def get_convovled_data(data,convolutions_1d):
+#    for conv in convolutions_1d:
+#        yield data[:,conv,:]
 
 
 class AlexPreprocess:
@@ -15,30 +15,45 @@ class AlexPreprocess:
     def __init__(self):
         pass
 
-    def extract_expert_features(self,data,convolution_indeces):
+    
+    def extract_expert_features(self,data,convolution_indeces,ch_dim):
         #TODO too slow, make parallel feature computations
-        phase_data = self._calc_local_phase(data)
+        assert(data.ndim == 3)
+        time_dim = data.ndim - ch_dim
+        phase_data = self._calc_local_phase(data,time_dim)
         beta = 1
         trials,features_length,time_length = data.shape[0],len(convolution_indeces),data.shape[-1]
-        res = np.zeros((trials,features_length,time_length))
-        for index, data_chunk4conv in enumerate(get_convovled_data(phase_data,convolution_indeces)):
-            res[:,index,:]=self._get_Alex_feature(data_chunk4conv, beta)
+        res_shape = list(data.shape)
+        res_shape[ch_dim] = len(convolution_indeces)
+        res = np.zeros(res_shape)
+        for index, data_chunk4conv in enumerate(self._get_convolved_data(phase_data,convolution_indeces,ch_dim)):
+            indth = [slice(None)] * ch_dim + [index] + [slice(None)] * (data.ndim - ch_dim - 1)
+            res[indth]=self._get_Alex_feature(data_chunk4conv, beta, feat_dim=ch_dim)
         return res
 
 
 
-    def _calc_local_phase(self,data):            #TODO rewrite as generators for large amount of data
-        analytic_signal = hilbert(data)
+    def _calc_local_phase(self,data,time_dim):    
+        analytic_signal = hilbert(data,axis = time_dim)
         return np.unwrap(np.angle(analytic_signal))
 
-
-    def _get_Alex_feature(self,conv_phase_values,beta):
-        conv_length = conv_phase_values.shape[-2]
+    def _get_convolved_data(self,data,convolutions_1d,ch_dim):
+        for convolution in convolutions_1d:
+            ind = [slice(None)] * ch_dim + [convolution] + [slice(None)] * (data.ndim - ch_dim - 1)
+            yield data[ind]
+    
+    def _get_Alex_feature(self,conv_phase_values,beta,feat_dim):
+        conv_length = conv_phase_values.shape[feat_dim]
         comb = list(combinations(range(conv_length), 2))
-        conv_phase_delta = np.zeros((conv_phase_values.shape[0],len(comb),conv_phase_values.shape[2]))
+        out_shape = list(conv_phase_values.shape)
+        out_shape[feat_dim]=len(comb)
+        conv_phase_delta = np.zeros(out_shape)
         for ind,(i,j) in enumerate(comb):
-            conv_phase_delta[:,ind,:] = conv_phase_values[:,i,:]-conv_phase_values[:,j,:]
-        return np.exp(-beta * norm(conv_phase_delta,axis = 1) ** 2)
+            ith = [slice(None)] * feat_dim + [i] + [slice(None)] * (conv_phase_values.ndim - feat_dim - 1)
+            jth = [slice(None)] * feat_dim + [j] + [slice(None)] * (conv_phase_values.ndim - feat_dim - 1)
+            indth = [slice(None)] * feat_dim + [ind] + [slice(None)] * (conv_phase_values.ndim - feat_dim - 1)
+            conv_phase_delta[indth] = conv_phase_values[ith]-conv_phase_values[jth]
+        return np.exp(-beta * norm(conv_phase_delta,axis = feat_dim) ** 2)
 
 
     def visualise_activations(self,vis_object,extracted_features,convs_indices):
@@ -52,12 +67,13 @@ class AlexPreprocess:
 
 
 if __name__ == '__main__':
-    synth_data = GSN128Data()
-    data = synth_data.load_all_data()[[0],:,:]
-    cn = ConvolutionsGSN128()
-    convs = cn.get_1Dconvolution_channels(3)
-    ap = AlexPreprocess()
-    features = AlexPreprocess().extract_expert_features(data,convs)
+    data_source = NeuromagData('mag')
+    X, _ = data_source.get_data_from_exp('em01',target_dim_order=['trial', 'time', 'channel'])
+    dev = Neuromag('mag')
+    cn = Convolutions(dev)
+    convs = cn.get_1Dconvolutions(3)
+    # ap = AlexPreprocess()
+    features = AlexPreprocess().extract_expert_features(X,convs,ch_dim=2)
     vc = VisualisationConvolutions(cn)
-    ap.visualise_activations(vc,features,convs)
+    # ap.visualise_activations(vc,features,convs)
     print 'ok'
--- Alex_nn.py
+++ Alex_nn.py
@@ -6,6 +6,9 @@ from keras.callbacks import EarlyStopping,TensorBoard,ReduceLROnPlateau
 from keras import backend as K
 from scipy.signal import resample
 from uuid import uuid4
+from convolutions import *
+from conv_trees import *
+from AlexPreprocess import *
 
 
 def get_base_model(input_len, fsize,channel_number):
@@ -55,20 +58,41 @@ def get_resampled_data(data,axis):
     epoch_len = data.shape[axis]
     return [resample(data,epoch_len/4,axis=axis),resample(data,epoch_len/2,axis=axis),data]
 
+def data_placement(dat,convs,conv_seqs):
+    # @data_trial of shape (trials x time x convs)
+    # @convs_seqs - np.array of (number of seqs x seq length)
+    seq_lenght = conv_seqs.shape[1]
+    time_len = (data.shape[1] - (seq_lenght-1))*seq_lenght
+    res = np.zeros(data.shape[0],time_len,len(conv_seqs))
+    res_time_counter = 0
+    for t in range(data_trial.shape[0]-seq_length)
+        for dt in range(seq_length):
+			res[:,res_time_counter,:] = data[:,t+dt,conv_seqs[:,dt]]
+	return res
+
 if __name__=='__main__':
+
     data_source = NeuromagData('mag')
     dim_order = ['trial','time','channel']
     X,y=data_source.get_all_experiments_data(dim_order)
     dev = Neuromag('mag')
-    augmenter = DataAugmentation(device=dev)
-    Xm = augmenter.mirror_sensors(X)
-    X = np.concatenate((X,Xm),axis=0)
-    y = np.hstack((y,y))
-    X = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
-    model = get_full_model(epoch_len = X.shape[1],channel_number = X.shape[2])
-    nb_epoch = 10000
-    early_stopping = EarlyStopping(monitor='val_loss', patience=100, verbose=0, mode='auto')
-    tensor_board = TensorBoard(log_dir = './logs/'+str(uuid4()), histogram_freq = 3)
-    with K.tf.device('/gpu:2'):
-        model.fit(x=get_resampled_data(X,axis=1),y=to_onehot(y),batch_size=30, nb_epoch = nb_epoch,
-                            callbacks=[tensor_board], verbose=1, validation_split=0.2,shuffle=True)
+    convs = Convolutions(dev)
+    conv_seqs = cc.make_geodesic_conv_combinations(dev.topography_3D, np.array(convs), 3, 0.4, 0.4, 0.3, cc.curr_faces,
+                                             'directions_Real.csv')
+    conv_seqs = filter(lambda x: x[1] < 0.15, conv_seqs)
+    data = AlexPreprocess().extract_expert_features(X,convs)
+    
+	
+
+    # augmenter = DataAugmentation(device=dev)
+    # Xm = augmenter.mirror_sensors(X)
+    # X = np.concatenate((X,Xm),axis=0)
+    # y = np.hstack((y,y))
+    # X = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
+    # model = get_full_model(epoch_len = X.shape[1],channel_number = X.shape[2])
+    # nb_epoch = 10000
+    # early_stopping = EarlyStopping(monitor='val_loss', patience=100, verbose=0, mode='auto')
+    # tensor_board = TensorBoard(log_dir = './logs/'+str(uuid4()), histogram_freq = 3)
+    # with K.tf.device('/gpu:2'):
+    #     model.fit(x=get_resampled_data(X,axis=1),y=to_onehot(y),batch_size=30, nb_epoch = nb_epoch,
+    #                         callbacks=[tensor_board], verbose=1, validation_split=0.2,shuffle=True)
